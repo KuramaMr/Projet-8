48e6639b123a86c65f631e48cf350f4484fd9134
Maquet Dorian <takenzmr@gmail.com>
2023-12-04 11:49:09 +0100

    Update

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-function Home() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +22,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default App;
