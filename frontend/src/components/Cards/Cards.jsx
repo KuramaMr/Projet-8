7d50cf44558b5c215c40d6f9f5d0dec6cc2cd4b8
Maquet Dorian <takenzmr@gmail.com>
2023-12-14 01:47:32 +0100

    Update de la nuit, Bonjour !

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,9 +1,28 @@
 import './Cards.css'
+import React from 'react'
+import { Link } from "react-router-dom"
+import records from "../Datas.json"
 
-function Cards() {
-    return(
-        <div className='grid'> 
+// composant qui permet de récupérer la liste des logements et de les afficher sous forme de card
 
+const Cards = () => {
+    return (
+        <div className="logements">
+
+            {/* liste la base de données */}
+            {records.map((record) => {
+                const { id, cover, title } = record;
+
+                // affiche la fiche logement sur la page d'accueil
+                return (
+                    <div className="fiche-logement" key={id}>
+                        <Link to={{ pathname: "/LogementFiche", search: "?_id="+id }}>
+                            <img src={cover} alt={title} />
+                            <h3>{title}</h3>
+                        </Link>
+                    </div>
+                )
+            })}
         </div>
     )
 }
