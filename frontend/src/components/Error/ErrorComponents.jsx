8480d9c6446a4ef5d2bfe232e5077be6f06dc85c
Maquet Dorian <takenzmr@gmail.com>
2023-12-31 14:26:21 +0100

    Avancement page Erreur

--- frontend/src/components/Error/ErrorComponents.jsx
+++ frontend/src/components/Error/ErrorComponents.jsx
@@ -1,13 +1,18 @@
 import './Error.css';
 import { Link } from 'react-router-dom';
+import NavBar from "../Navbar/NavBar.jsx";
+import Footer from "../Footer/Footer.jsx";
 
 function ErrorComponents() {
     return(
+        
         <div className='404Container'>
+            <NavBar />
             <p>404</p>
             <p>Oups ! La page que vous demandez n'existe pas.</p>
             <Link to={"/"}>
             </Link>
+            <Footer />
         </div>
     )
     
