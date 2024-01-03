d9c697106ac23acfa812a3aabed8ba9d34aae691
Maquet Dorian <takenzmr@gmail.com>
2024-01-03 08:59:21 +0100

    Media CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import LogementFiche from './pages/LogementFiche.jsx';
 import Error from './pages/Error.jsx';
-import './styles/index.css'
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
